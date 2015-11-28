0e2c8fd299a0a4ec998a935f0181a7abf3c9cf4d
Marcin Krawczyk <marcinn.krawczyk@gmail.com>
2015-11-28 22:09:32 +0100

    added less gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var less = require('gulp-less-sourcemap');
 
 gulp.task('test:unit', function () {
     return gulp.src('test/unit/**/*.spec.js', {read: false})
         .pipe(mocha({reporter: 'nyan'}));
+});
+
+gulp.task('less', function () {
+    gulp.src('./public/css/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('./public/css'));
 });
