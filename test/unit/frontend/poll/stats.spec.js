0f751af2d191fbf041f48a8766be72a9217c95c0
marcin krawczyk <marcinn.krawczyk@gmail.com>
2016-07-05 23:00:25 +0200

    fixed unit tests.

--- test/unit/frontend/poll/stats.spec.js
+++ test/unit/frontend/poll/stats.spec.js
@@ -322,7 +322,7 @@ describe('#calculate stats tests', function () {
 		});
 	});
 
-    it.only('#calculateStats5', function () {
+    it('#calculateStats5', function () {
         var input = {
             "data": {
                 "votes": {
